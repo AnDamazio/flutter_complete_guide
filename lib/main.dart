d166c19fac682c158cb7db5f0358e3db354469c3
AnDamazio <damazioandre@hotmail.com>
2022-07-01 13:51:39 -0300

    Add column with buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('My First App'),
         ),
-        body: const Text('This is my default text!'),
+        body: Column(
+          children: const [
+            Text('The question!'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 3'),
+            )
+          ],
+        ),
       ),
     );
   }
