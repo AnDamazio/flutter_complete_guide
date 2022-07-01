ddc202d6552fffd3c6861f4d413c1da5df4030c7
AnDamazio <damazioandre@hotmail.com>
2022-07-01 13:34:03 -0300

    Add navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello, Ryan!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: const Text('This is my default text!'),
+      ),
     );
   }
 }
